1925815bae0885be89c87242d7944baa3e921c45
wangsongwen <2544894086wsw@gmail.com>
2020-09-28 17:37:14 +0800

    first commit

---
+++ src/main/java/com/wsw/springbootquartz/job/ApiJob.java
@@ -0,0 +1,17 @@
+package com.wsw.springbootquartz.job;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+/**
+ * @Author WangSongWen
+ * @Date: Created in 17:31 2020/9/28
+ * @Description:
+ */
+public class ApiJob implements Job {
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+
+    }
+}
---
+++ src/test/java/com/wsw/springbootquartz/SpringbootquartzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsw.springbootquartz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootquartzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
