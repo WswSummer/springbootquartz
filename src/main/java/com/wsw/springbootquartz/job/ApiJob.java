84673f0fd45f8d611e649f128341ab232f0c5ef1
wangsongwen <2544894086wsw@gmail.com>
2020-09-28 17:49:56 +0800

    second commit

--- src/main/java/com/wsw/springbootquartz/job/ApiJob.java
+++ src/main/java/com/wsw/springbootquartz/job/ApiJob.java
@@ -1,17 +1,21 @@
 package com.wsw.springbootquartz.job;
 
+import lombok.extern.slf4j.Slf4j;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
+import java.util.Date;
+
 /**
  * @Author WangSongWen
  * @Date: Created in 17:31 2020/9/28
- * @Description:
+ * @Description: 实现定时功能的接口：如定时推送信息等
  */
+@Slf4j
 public class ApiJob implements Job {
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-
+        log.info(new Date() + "正在执行...");
     }
 }
